012bbfcad12430dfcb97a4f4dc49d716329338e1
Ever Fernando Garay Molinas <everfgmolinas@hotmail.com>
2022-05-22 23:54:39 -0400

    feat: user to json and from json

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -5,7 +5,22 @@ class User {
     lastName;
 
   User(
-      {this.givenName,
+      {this.id,
+        this.givenName,
         this.lastName,
         this.identifier,});
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+    id: json["id"],
+    identifier: json["identifier"],
+    lastName: json["lastName"],
+    givenName: json["givenName"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "givenName": givenName,
+    "lastName": lastName,
+    "identifier": identifier,
+  };
 }
