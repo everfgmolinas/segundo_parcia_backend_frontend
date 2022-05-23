a4c047bd6919482a97b08bc693536eeff3d9cfc6
Ever Fernando Garay Molinas <everfgmolinas@hotmail.com>
2022-05-22 23:59:13 -0400

    feat: repository for connect to backend

---
+++ lib/network/user_repository.dart
@@ -0,0 +1,36 @@
+import 'package:dio/dio.dart';
+import 'package:segundo_parcia_backend/constants.dart';
+import 'package:segundo_parcia_backend/models/restaurant_model.dart';
+import 'package:segundo_parcia_backend/network/repository_helper.dart';
+
+import 'http.dart';
+
+class UserRepository {
+  Future<List<Restaurant>>? getListRestaurant() async {
+    try {
+      print('get restaurants');
+      Response response =
+          await dio.get("/restaurante/");
+      if (response.statusCode == 200) {
+        print('vaccinate geted');
+        final userVaccination = userVaccinationFromJson(response.data);
+        return userVaccination;
+      } else if (response.statusCode == 404) {
+        throw Failure(
+            "No existe el ciudadano con los datos proveidos en la BD de vacunación");
+      }
+      throw Failure(genericError);
+    // } catch (e) {
+    //   print(e);
+    //   throw Failure(genericError);
+    // }
+    } on DioError  catch (ex) {
+      if (ex.type == DioErrorType.connectTimeout || ex.type == DioErrorType.receiveTimeout) {
+        throw Failure("Ups, parece que tienes conexión a internet muy lenta");
+      }else {
+        throw Failure(genericError);
+      }
+    }
+  }
+
+}
