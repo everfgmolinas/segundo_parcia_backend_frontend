a8e3acb9c7fdd900b382ce318baa77e9e6f4adeb
elias7991 <78571779+elias7991@users.noreply.github.com>
2022-05-28 22:53:58 -0400

    feat:possible reservations

--- lib/pages/gestor_pages/restaurant_pages.dart
+++ lib/pages/gestor_pages/restaurant_pages.dart
@@ -14,22 +14,59 @@ class Restaurants extends StatefulWidget {
 
 class StateRestaurants extends State<Restaurants> {
 
+  Map<String, bool> values = {
+    '12:00 - 13:00': false,
+    '13:00 - 14:00': false,
+    '14:00 - 15:00': false,
+    '15:00 - 16:00': false,
+    '16:00 - 17:00': false,
+    '17:00 - 18:00': false,
+    '18:00 - 19:00': false,
+    '19:00 - 20:00': false,
+    '20:00 - 21:00': false,
+    '21:00 - 22:00': false,
+    '22:00 - 23:00': false,
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: widget.restaurantName!=null
-            ?Text('${widget.restaurantName}')
+            ?Text('${widget.restaurantName}, ${widget.restaurantAddress}')
             : Text('Restaurante'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
+      body: Column(
+        children: [
+          Text(''),
+          Center(
+            child: Text(
+              '¿En qué horario desea reservar?',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ),
+          Text(''),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(10),
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              children: values.keys.map((String key) {
+                return new CheckboxListTile(
+                  title: Text(key),
+                  value: values[key],
+                  onChanged: (value) {
+                    setState(() {
+                      values[key] = value!;
+                    });
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+        ],
       ),
     );
   }
