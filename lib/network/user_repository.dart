2e1eaec2980bf4b8c2e68b3914ad5f5b1a77bf90
Ever Fernando Garay Molinas <everfgmolinas@hotmail.com>
2022-06-22 07:01:28 -0400

    feat: add connections with backend

--- lib/network/user_repository.dart
+++ lib/network/user_repository.dart
@@ -1,9 +1,20 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:segundo_parcia_backend/constants.dart';
+import 'package:segundo_parcia_backend/models/consumo_model.dart';
+import 'package:segundo_parcia_backend/models/detalle_model.dart';
 import 'package:segundo_parcia_backend/models/hour_model.dart';
+import 'package:segundo_parcia_backend/models/producto_model.dart';
 import 'package:segundo_parcia_backend/models/restaurant_model.dart';
 import 'package:segundo_parcia_backend/models/table_model.dart';
+import 'package:segundo_parcia_backend/models/user_model.dart';
 import 'package:segundo_parcia_backend/network/repository_helper.dart';
+import 'package:open_file/open_file.dart' as open;
 
 import 'http.dart';
 
@@ -92,6 +103,152 @@ class UserRepository {
       throw Failure(genericError);
     }
 
+  }
+
+  Future<List<MyTable>>? getListTables(String restaurant_id) async {
+    try {
+      List<MyTable> tables = [];
+      print('get table');
+      Response response =
+      await dio.get("/mesa/restid/$restaurant_id");
+      if (response.statusCode == 200) {
+        print('table geted');
+        final listTable = tableFromJson(response.data);
+        return listTable;
+      } else if (response.statusCode == 404) {
+        throw Failure(
+            "No existe el ciudadano con los datos proveidos en la BD de vacunación");
+      }
+      throw Failure(genericError);
+      // } catch (e) {
+      //   print(e);
+      //   throw Failure(genericError);
+      // }
+    } catch(e) {
+      print(e);
+      throw Failure(genericError);
+    }
+
+  }
+
+  Future<List<Consumes>>? getConsumes(String mesa_id) async {
+    try {
+      print('get table');
+      Response response =
+      await dio.get("/consumo");
+      if (response.statusCode == 200) {
+        print('consumes geted');
+        response.data = response.data.where((e) => e['estado'] == 'Abierto').toList();
+        response.data = response.data.where((e) => e['id_mesa'].toString() == mesa_id.toString()).toList();
+        final consumes = consumesFromJson(response.data);
+        return consumes;
+      } else if (response.statusCode == 404) {
+        throw Failure(
+            "No existen consumes");
+      }
+      throw Failure(genericError);
+      // } catch (e) {
+      //   print(e);
+      //   throw Failure(genericError);
+      // }
+    } catch(e) {
+      print(e);
+      throw Failure(genericError);
+    }
+
+  }
+
+  Future<List<Producto>>? getProducts() async {
+    try {
+      print('get product');
+      Response response =
+      await dio.get("/producto");
+      if (response.statusCode == 200) {
+        print('products geted');
+        final producto = productFromJson(response.data);
+        return producto;
+      } else if (response.statusCode == 404) {
+        throw Failure(
+            "No existen consumes");
+      }
+      throw Failure(genericError);
+      // } catch (e) {
+      //   print(e);
+      //   throw Failure(genericError);
+      // }
+    } catch(e) {
+      print(e);
+      throw Failure(genericError);
+    }
+
+  }
+
+  Future<List<Detalle>>? getDetails(String consumo_id) async {
+    try {
+      print('get details');
+      Response response =
+      await dio.get("/detalle/$consumo_id");
+      if (response.statusCode == 200) {
+        print('products geted');
+        final detalles = detailFromJson(response.data);
+        return detalles;
+      } else if (response.statusCode == 404) {
+        throw Failure(
+            "No existen detalles");
+      }
+      throw Failure(genericError);
+      // } catch (e) {
+      //   print(e);
+      //   throw Failure(genericError);
+      // }
+    } catch(e) {
+      print(e);
+      throw Failure(genericError);
+    }
+
+  }
+
+  Future<List<Cliente>>? getClientes() async {
+    try {
+      print('get clients');
+      Response response =
+      await dio.get("/cliente");
+      if (response.statusCode == 200) {
+        print('clients geted');
+        final clientes = clientesFromJson(response.data);
+        return clientes;
+      } else if (response.statusCode == 404) {
+        throw Failure(
+            "No existen clientes");
+      }
+      throw Failure(genericError);
+      // } catch (e) {
+      //   print(e);
+      //   throw Failure(genericError);
+      // }
+    } catch(e) {
+      print(e);
+      throw Failure(genericError);
+    }
+
+  }
+
+  Future<None>? downloadPdf(String consume_id) async {
+    try {
+      print('get user downloadVacinnationPdf');
+      final appStorage = await getApplicationDocumentsDirectory();
+      final file = File('${appStorage.path}/ticket${consume_id}.pdf');
+      final response = await dioDownloader
+          .get('/consumo/$consume_id/factura');
+      final raf = file.openSync(mode: FileMode.write);
+      raf.writeFromSync(response.data);
+      await raf.close();
+
+      open.OpenFile.open(file.path);
+      return None();
+    } catch (e) {
+      throw Failure(genericError);
+    }
   }
 
 }
